b6f4c5cf7fddb9ade6925b94daa61d55d1a960e6
Louis Gustavo <louis.gustavo@traveloka.com>
2025-07-20 21:02:39 +0700

    Fix robots and sitemap auto-generation

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,11 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://louisite.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        disallow: '',
+      },
+    ]
   }
 }
